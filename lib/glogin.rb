a70ac5bc47c0a20a2775a627f25c3b06d3df22d5
Yegor Bugayenko <yegor256@gmail.com>
2024-01-06 19:32:09 +0300

    year up

--- lib/glogin.rb
+++ lib/glogin.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 #
-# Copyright (c) 2017-2023 Yegor Bugayenko
+# Copyright (c) 2017-2024 Yegor Bugayenko
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the 'Software'), to deal
@@ -28,7 +28,7 @@ require_relative 'glogin/cookie'
 
 # GLogin main module.
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2017-2023 Yegor Bugayenko
+# Copyright:: Copyright (c) 2017-2024 Yegor Bugayenko
 # License:: MIT
 module GLogin
 end
