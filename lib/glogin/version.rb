974a49776c58befb18a93c08e157ed76319b7b19
Rultor.com <gpg@rultor.com>
2025-12-22 02:41:46 +0000

    version set to 0.17.1

--- lib/glogin/version.rb
+++ lib/glogin/version.rb
@@ -12,11 +12,11 @@ module GLogin
   #
   # @example Check the gem version
   #   puts GLogin::VERSION
-  #   # => "0.0.0"
+  #   # => "0.17.1"
   #
   # @example Version comparison
   #   if Gem::Version.new(GLogin::VERSION) >= Gem::Version.new('0.1.0')
   #     puts "Using GLogin version 0.1.0 or newer"
   #   end
-  VERSION = '0.0.0'
+  VERSION = '0.17.1'
 end
